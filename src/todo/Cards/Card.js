b703498623abe025c8130dab8989ffe86aa63344
shehzadsaifullah <shehzadsaifullah@gmail.com>
2022-03-14 16:35:45 +0500

    perfect delete

--- src/todo/Cards/Card.js
+++ src/todo/Cards/Card.js
@@ -2,17 +2,16 @@ import { useState, useEffect } from "react";
 import classes from "./Card.module.css";
 
 const Card = (props) => {
-  const recievedPropsList = props.value;
-
-  const [newList, setNewValue] = useState([]);
+  const recievedData = props.value;
+  const [itemsList, setItemsList] = useState([]);
 
   useEffect(() => {
-    setNewValue(recievedPropsList);
-  }, []);
+    setItemsList(recievedData);
+  }, [recievedData]);
 
   const deleteHandler = (id) => {
-    const list = props.value.filter((entry) => entry.id !== id);
-    setNewValue(list);
+    const list = itemsList.filter((entry) => entry.id !== id);
+    setItemsList(list);
   };
 
   const editHandler = () => {};
@@ -20,7 +19,7 @@ const Card = (props) => {
   return (
     <div className={classes.card}>
       <ul>
-        {newList.map((entry) => (
+        {itemsList.map((entry) => (
           <li key={entry.id}>
             {entry.listItem}
             <button className={classes.button} onClick={editHandler}>
--- src/todo/Todo.js
+++ src/todo/Todo.js
@@ -1,24 +1,20 @@
 import Card from "./Cards/Card";
 import Input from "./InputForm/InputDetail";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "../App.module.css";
 
 const Todo = () => {
   const [recievedData, setRecievedData] = useState([]);
+
   const dataRecieverHandler = (todoItem) => {
     setRecievedData((todolist) => {
       return [...todolist, { id: Math.random(), listItem: todoItem }];
     });
-    console.log(recievedData);
   };
-  /*
-  (todolist) => {
-      return [
-        ...todolist,
-        { id: Math.random().toString(), listItem: todoItem },
-      ];
-    }
-  */
+
+  useEffect(() => {
+    console.log(recievedData);
+  }, [recievedData]);
 
   return (
     <div className={classes.main}>
